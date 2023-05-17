f19ae3d7a00ffe42aeb18d6dfa657fd63ad5f359
RenasAli <rena0506@stud.kea.dk>
2023-05-17 18:00:13 +0200

    ff

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const button = document.getElementById("button");
+ // Check if the browser supports notifications
+        if ('Notification' in window && 'serviceWorker' in navigator) {
+            // Request permission for notifications
+            Notification.requestPermission().then(function (permission) {
+                if (permission === 'granted') {
+                    console.log('Notification permission granted.');
+                } else {
+                    console.log('Notification permission denied.');
+                }
+            });
+        }
 
 button.addEventListener("click", ()=>{
-    Notification.requestPermission().then(perm => {
-        if(perm === "granted"){
-            const notification = new Notification("Test Notification", {
-                body: " hej",
-                data: {hello: "world"}
-                
-            })
+   if ('Notification' in window && 'serviceWorker' in navigator) {
+            // Request permission for notifications
+            Notification.requestPermission().then(function (permission) {
+                if (permission === 'granted') {
+                    console.log('Notification permission granted.');
+                } else {
+                    console.log('Notification permission denied.');
+                }
+            });
         }
-    }
-
-    )
 })
